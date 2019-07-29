50dbd64b22678b21bae7a4f53604ee01c808d19b
lighty <hikalin8686@gmail.com>
2019-07-29 23:46:52 +0900

    画像ファイルを表示

--- app.rb
+++ app.rb
@@ -1,11 +1,11 @@
 require 'sinatra'
 require 'cgi'
 
-get '/screenshot' do
-  "url: #{params['url']}"
-end
-
 filename = CGI.escape(params['url']) + '.png'
 Tempfile.create(filename) do |f|
   system(ENV.fetch('GOOGLE_CHROME_SHIM'), "--screenshot=#{f.path}", "--window-size=1480,1110", params['url'])
+end
+
+get '/screenshot' do
+  "url: #{params['url']} <img src='/#{filename}' />"
 end
