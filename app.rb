d2ddf09d2871ca7e2ad086aa776b16a06d8bfb1a
lighty <hikalin8686@gmail.com>
2019-07-29 23:51:18 +0900

    fix bug

--- app.rb
+++ app.rb
@@ -1,11 +1,16 @@
 require 'sinatra'
 require 'cgi'
 
-filename = CGI.escape(params['url']) + '.png'
-Tempfile.create(filename) do |f|
-  system(ENV.fetch('GOOGLE_CHROME_SHIM'), "--screenshot=#{f.path}", "--window-size=1480,1110", params['url'])
+def take_screenshot(url)
+  filename = CGI.escape(url) + '.png'
+  Tempfile.create(filename) do |f|
+    system(ENV.fetch('GOOGLE_CHROME_SHIM'), "--screenshot=#{f.path}", "--window-size=1480,1110", url)
+  end
+  filename
 end
 
 get '/screenshot' do
-  "url: #{params['url']} <img src='/#{filename}' />"
+  url = params['url']
+  filename = take_screenshot(url)
+  "url: #{url} <img src='/#{filename}' />"
 end
