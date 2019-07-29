47091933e777128fdd7deb31f85cb7495f621306
lighty <hikalin8686@gmail.com>
2019-07-29 23:45:11 +0900

    クエリパラメータで指定されたURLのスクショを撮る

--- app.rb
+++ app.rb
@@ -1,8 +1,11 @@
 require 'sinatra'
+require 'cgi'
 
 get '/screenshot' do
   "url: #{params['url']}"
 end
 
-  
-# cmd = "#{headless_chrome_exe} --headless --no-sandbox --disable-gpu --screenshot=#{f.path} --window-size=1480,1110 #{self.url}"
+filename = CGI.escape(params['url']) + '.png'
+Tempfile.create(filename) do |f|
+  system(ENV.fetch('GOOGLE_CHROME_SHIM'), "--screenshot=#{f.path}", "--window-size=1480,1110", params['url'])
+end
