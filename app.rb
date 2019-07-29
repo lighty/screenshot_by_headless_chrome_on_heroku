987acf41350e61069920b1d4ddde0618f01201e4
lighty <hikalin8686@gmail.com>
2019-07-29 23:59:41 +0900

    fix bug

--- app.rb
+++ app.rb
@@ -3,14 +3,15 @@ require 'cgi'
 
 def take_screenshot(url)
   filename = CGI.escape(url) + '.png'
-  Tempfile.create(filename) do |f|
-    system(ENV.fetch('GOOGLE_CHROME_SHIM'), "--screenshot=#{f.path}", "--window-size=1480,1110", url)
-  end
+  directory = './public/'
+  command = ENV.fetch('GOOGLE_CHROME_BIN')
+  options = %W(--headless --no-sandbox --disable-gpu --screenshot=#{directory + filename} --window-size=1000,1000)
+  system(command, *options, url)
   filename
 end
 
 get '/screenshot' do
   url = params['url']
   filename = take_screenshot(url)
-  "url: #{url} <img src='/#{filename}' />"
+  "url: #{url} <img src='/#{URI.encode(filename)}' />"
 end
